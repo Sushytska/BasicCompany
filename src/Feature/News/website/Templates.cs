658c7e74e9ccbf9529aeb5bf1cbfe352e947ab66
Valentyna Sushytska <valentyna.sushytska@valtech.com>
2021-11-04 16:52:33 +0200

    Adding related items to Detail page

--- src/Feature/News/website/Templates.cs
+++ src/Feature/News/website/Templates.cs
@@ -14,6 +14,7 @@ namespace BasicCompany.Feature.News
                 public static readonly ID NewsHeaderImage = new ID("{5629D19F-0B55-40CA-AD15-5219A6A5C0FE}");
                 public static readonly ID NewsContent = new ID("{8EA70858-0096-4184-989A-84F7BA837056}");
                 public static readonly ID NewsAuthor = new ID("{B0DD1564-B311-47F2-BFDD-928806959B8A}");
+                public static readonly ID NewsRelated = new ID("{531EA361-5C1D-406F-94A8-97786B36C568}");
             }
         }
     }
