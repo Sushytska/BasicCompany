e4d9da558155115a24934509a634e490c27d84a1
Yakiv Artiukhov <yakiv.artiukhov@gmail.com>
2022-01-14 13:25:50 +0200

    Creating Forms Page Form with a custom submit action, that logs a successful attempt.

---
+++ src/Feature/Forms/website/BasicCompany.Feature.Forms/Models/LogSubmit.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using static System.FormattableString;
+
+namespace BasicCompany.Feature.Forms.Models
+{
+    /// <summary>
+    /// Executes a submit action for logging the form submit status.
+    /// </summary>
+    /// <seealso cref="Sitecore.ExperienceForms.Processing.Actions.SubmitActionBase{TParametersData}" />
+    public class LogSubmit : SubmitActionBase<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogSubmit"/> class.
+        /// </summary>
+        /// <param name="submitActionData">The submit action data.</param>
+        public LogSubmit(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+        }
+
+        /// <summary>
+        /// Tries to convert the specified <paramref name="value" /> to an instance of the specified target type.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="target">The target object.</param>
+        /// <returns>
+        /// true if <paramref name="value" /> was converted successfully; otherwise, false.
+        /// </returns>
+        protected override bool TryParse(string value, out string target)
+        {
+            target = string.Empty;
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the action with the specified <paramref name="data" />.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="formSubmitContext">The form submit context.</param>
+        /// <returns>
+        ///   <c>true</c> if the action is executed correctly; otherwise <c>false</c>
+        /// </returns>
+        protected override bool Execute(string data, FormSubmitContext formSubmitContext)
+        {
+            Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
+
+            if (!formSubmitContext.HasErrors)
+            {
+                Logger.Info(Invariant($"Form {formSubmitContext.FormId} submitted successfully."), this);
+            }
+            else
+            {
+                Logger.Warn(Invariant($"Form {formSubmitContext.FormId} submitted with errors: {string.Join(", ", formSubmitContext.Errors.Select(t => t.ErrorMessage))}."), this);
+            }
+
+            return true;
+        }
+    }
+}
